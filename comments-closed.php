01ad0ea5d827177e8fd26a56474e590a65490824
plus-cctv <53801822+plus-cctv@users.noreply.github.com>
2019-08-05 20:09:52 -0700

    Add files via upload

---
+++ comments-closed.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Comments closed.
+ *
+ * @package plus
+ */
+
+// If comments are closed and there are comments, let's leave a little note, shall we?
+if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+?>
+	<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'plus' ); ?></p>
+<?php endif; ?>
