355d967c834087fbdbb688053d8e67308c287c2d
plus-cctv <53801822+plus-cctv@users.noreply.github.com>
2019-08-08 17:59:11 +0300

    Update comments-navigation.php

--- comments-navigation.php
+++ comments-navigation.php
@@ -2,16 +2,16 @@
 /**
  * Comments navigation.
  *
- * @package Bootswatch
+ * @package Plus
  */
 
 if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 	<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
-		<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'bootswatch' ); ?></h2>
+		<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'plus' ); ?></h2>
 		<div class="nav-links">
 
-			<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'bootswatch' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'bootswatch' ) ); ?></div>
+			<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', 'plus' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', 'plus' ) ); ?></div>
 
 		</div><!-- .nav-links -->
 	</nav><!-- #comment-nav-above -->
