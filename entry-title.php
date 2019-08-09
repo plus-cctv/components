b404ed6d53c00317a59984fc7325d49ef7b52d18
Plus! Team <53801822+plus-cctv@users.noreply.github.com>
2019-08-09 03:36:34 +0300

    Update entry-title.php

--- entry-title.php
+++ entry-title.php
@@ -2,11 +2,11 @@
 /**
  * Snippet.
  *
- * @package Bootswatch
+ * @package plus
  */
 
 the_title(
-	sprintf( '<%1$s class="entry-title"><a href="%2$s" rel="bookmark">'
+	sprintf( '<%1$s class="entry-title"><a href="%2$s" rel="plus">'
 		, is_singular() ? 'h1' : 'h2'
 		, esc_url( get_permalink() )
 	),
