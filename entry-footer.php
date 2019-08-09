c05e5cb5763f5344e8ef9bd9790151a1cfd21180
Plus! Team <53801822+plus-cctv@users.noreply.github.com>
2019-08-09 03:38:58 +0300

    Update entry-footer.php

--- entry-footer.php
+++ entry-footer.php
@@ -2,7 +2,7 @@
 /**
  * Snippet.
  *
- * @package Bootswatch
+ * @package Plus
  */
 
 if ( ! current_user_can( 'edit_post', $post->ID ) ) {
@@ -15,7 +15,7 @@ if ( ! current_user_can( 'edit_post', $post->ID ) ) {
 		edit_post_link(
 			sprintf(
 				/* translators: %s: Post title */
-				esc_html__( 'Edit %s', 'bootswatch' ),
+				esc_html__( 'Edit %s', 'plus' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			),
 			'<span class="edit-link">',
